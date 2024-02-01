23a850d3266fbd8dfe8e1af72174a52072c9dd70
Fernando Salom <fer@rudo.es>
2024-02-01 21:08:29 +0100

    Fix token entity

--- moshimoshi/src/main/java/com/moshimoshi/network/entities/Token.kt
+++ moshimoshi/src/main/java/com/moshimoshi/network/entities/Token.kt
@@ -1,7 +1,10 @@
 package com.moshimoshi.network.entities
 import java.io.Serializable
 
-data class Token (
-    val value: String = "",
-    val expiresIn: Long = 0,
-): Serializable
+data class Token(
+    var value: String = "",
+    var timestampExpires: Long = 0,
+): Serializable {
+    var isValid: Boolean = false
+        get() = timestampExpires > 2
+}
