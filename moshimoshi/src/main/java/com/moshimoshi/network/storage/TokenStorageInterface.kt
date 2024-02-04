96e50f331fb2ae2cc8b7dba07e336e205046939d
Fernando Salom <fer@rudo.es>
2024-02-04 19:58:04 +0100

    Delete token store interface

--- moshimoshi/src/main/java/com/moshimoshi/network/storage/TokenStorageInterface.kt
+++
@@ -1,9 +0,0 @@
-package com.moshimoshi.network.storage
-
-import com.moshimoshi.network.entities.Token
-
-interface TokenStorageInterface {
-    val accessToken: Token
-    val refreshToken: Token
-    val idToken: Token
-}
