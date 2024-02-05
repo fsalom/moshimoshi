0dbdbcd9dbf158fa6ff22b018592243ce36d239d
Fernando Salom <fer@rudo.es>
2024-02-05 17:31:52 +0100

    Fix dependencies

--- app/src/main/java/com/moshimoshi/app/MainActivity.kt
+++ app/src/main/java/com/moshimoshi/app/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.moshimoshi.app
 
+import android.app.Application
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,20 +10,36 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.preferencesDataStore
+import com.moshimoshi.app.api.CharacterApi
 import com.moshimoshi.app.ui.theme.MoshiMoshiTheme
 import com.moshimoshi.network.MoshiMoshi
 import com.moshimoshi.network.authenticationcard.AuthenticationCard
+import com.moshimoshi.network.authenticationcard.api.APIAuthenticationImpl
 import com.moshimoshi.network.authenticator.Authenticator
+import com.moshimoshi.network.authenticator.AuthenticatorImpl
 import com.moshimoshi.network.interceptor.AuthInterceptor
+import com.moshimoshi.network.retrofit.Authenticated
 import com.moshimoshi.network.storage.datastore.TokenDataStoreImpl
-import kotlinx.coroutines.flow.internal.NoOpContinuation.context
-import java.util.prefs.Preferences
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import okhttp3.Request
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+
+
 
 class MainActivity : ComponentActivity() {
+    val Context.dataStore by preferencesDataStore(
+    name = "settings")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -31,20 +49,50 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val Context.dataStore by preferencesDataStore(
-                        name = "settings"
-                    )
-                    var datastore = DataStore<Preferences> by preferencesDataStore(name = "settings")
-                    var tokenStore = TokenDataStoreImpl(
+
+                    val tokenStore = TokenDataStoreImpl(
                         uniqueIdentifier = "APP",
-                        dataStore = )
-                    var authenticationCard = AuthenticationCard()
-                    var authenticator = Authenticator(tokenStore = , card = authenticationCard)
-                    var authInterceptor = AuthInterceptor(authenticator = authenticator)
-                    MoshiMoshi(
-                        baseUrl = "",
-                        interceptor = ,
-                        )
+                        dataStore = dataStore)
+
+                    val loginRequest = Request.Builder()
+                        .url("https://publicobject.com/helloworld.txt")
+                        .build()
+
+                    val authenticationCard = APIAuthenticationImpl(
+                        loginCall = loginRequest,
+                        refreshCall = loginRequest,
+                        packageName = "com.moshimoshi.app",
+                        className = "com.moshimoshi.app.MainActivity",
+                        context = LocalContext.current)
+
+                    val authenticator = AuthenticatorImpl(
+                        tokenStore = tokenStore,
+                        card = authenticationCard)
+
+                    val authInterceptor = AuthInterceptor(
+                        authenticator = authenticator)
+
+                    val moshi = MoshiMoshi(
+                        baseUrl = "https://rickandmortyapi.com/api/",
+                        interceptor = authInterceptor,
+                        authenticator = authenticator)
+
+                    var call = moshi.create(CharacterApi::class.java)
+
+                    runBlocking {
+                        // Launch a coroutine
+                        val job = launch {
+                            var x = moshi.load {
+                                call.getCharacter(id = 1)
+                            }
+                        }
+
+                        // Wait for coroutine to complete
+                        job.join()
+                        println("Main function complete.")
+                    }
+
+
                     Greeting("Android")
                 }
             }
