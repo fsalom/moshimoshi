2e13e06178701ba5f26cff8292f32f0f07625773
Fernando Salom <fer@rudo.es>
2024-02-06 15:52:50 +0100

    Add tokensDTO

---
+++ moshimoshi/src/main/java/com/moshimoshi/network/authenticationcard/api/dto/TokensDTO.kt
@@ -0,0 +1,18 @@
+package com.moshimoshi.network.authenticationcard.api.dto
+
+import com.google.gson.annotations.SerializedName
+import com.moshimoshi.network.entities.Token
+import com.moshimoshi.network.entities.Tokens
+
+data class TokensDTO(
+    @SerializedName("access_token") val accessToken: String,
+    @SerializedName("expires_in") val expiresIn: Int,
+    @SerializedName("refresh_token") val refreshToken: String
+)
+
+fun TokensDTO.toDomain(): Tokens {
+    return Tokens(
+        accessToken = Token(value = accessToken, timestampExpires = System.currentTimeMillis() + expiresIn * 1000),
+        refreshToken = Token(value = refreshToken, timestampExpires = 0)
+    )
+}
