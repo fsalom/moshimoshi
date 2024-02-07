ecce6f9b2eedb0212c3c947d0cf7692e86100fdf
Fernando Salom <fer@rudo.es>
2024-02-07 19:39:14 +0100

    Add app container with authentication dependencies

---
+++ app/src/main/java/com/moshimoshi/app/di/Container.kt
@@ -0,0 +1,75 @@
+package com.moshimoshi.app.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import com.moshimoshi.network.MoshiMoshi
+import com.moshimoshi.network.authenticationcard.api.APIAuthenticationImpl
+import com.moshimoshi.network.authenticator.AuthenticatorImpl
+import com.moshimoshi.network.entities.Endpoint
+import com.moshimoshi.network.entities.Method
+import com.moshimoshi.network.entities.Parameter
+import com.moshimoshi.network.interceptor.AuthInterceptor
+import com.moshimoshi.network.storage.datastore.TokenDataStoreImpl
+import okhttp3.MultipartBody
+import okhttp3.Request
+
+class Container() {
+    companion object {
+        @Volatile
+        private var instance: Container? = null
+
+        lateinit var context: Context
+        lateinit var dataStore: DataStore<Preferences>
+
+        fun config(context: Context, dataStore: DataStore<Preferences>) {
+            this.context = context
+            this.dataStore = dataStore
+        }
+
+        fun getInstance() =
+            instance ?: synchronized(this) {
+                instance ?: Container().also { instance = it }
+            }
+    }
+
+    private val tokenStore = TokenDataStoreImpl(
+        uniqueIdentifier = "APP",
+        dataStore = dataStore)
+
+    private val loginEndpoint = Endpoint(
+        url = "https://dashboard-staging.rudo.es/auth/token",
+        method = Method.POST,
+        formParams = listOf(
+            Parameter(key = "client_id", value = "1gzyJeSOyjUOmbSHREbsothngkBMato1VypQz35D"),
+            Parameter(key = "client_secret", value = "ynM8CpvlDHivO1jma1Q3Jv1RIJraBbJ9EtK5XI3dw4RpkxDgi9cZnmJlQs0XzuVCGWCNwQd8qJKAHFrLdHlRRDIzx8B08HJ0Htu6XFzP4kTRTWYIPHuCpldjouJhKvoA"),
+            Parameter(key = "grant_type", value = "password")))
+
+    private val refreshEndpoint = Endpoint(
+        url = "https://dashboard-staging.rudo.es/auth/token",
+        method = Method.POST,
+        formParams = listOf(
+            Parameter(key = "client_id", value = "1gzyJeSOyjUOmbSHREbsothngkBMato1VypQz35D"),
+            Parameter(key = "client_secret", value = "ynM8CpvlDHivO1jma1Q3Jv1RIJraBbJ9EtK5XI3dw4RpkxDgi9cZnmJlQs0XzuVCGWCNwQd8qJKAHFrLdHlRRDIzx8B08HJ0Htu6XFzP4kTRTWYIPHuCpldjouJhKvoA"),
+            Parameter(key = "grant_type", value = "refresh_token")))
+
+    private val authenticationCard = APIAuthenticationImpl(
+        loginEndpoint = loginEndpoint,
+        refreshEndpoint = refreshEndpoint,
+        packageName = "com.moshimoshi.app",
+        className = "com.moshimoshi.app.presentation.login.LoginActivity",
+        context = context)
+
+    val authenticator = AuthenticatorImpl(
+        tokenStore = tokenStore,
+        card = authenticationCard)
+
+    private val authInterceptor = AuthInterceptor(
+        authenticator = authenticator)
+
+    val moshi = MoshiMoshi(
+        baseUrl = "https://rickandmortyapi.com/api/",
+        interceptor = authInterceptor,
+        authenticator = authenticator)
+}
