9602cb12e5c9fc36bdc8c946eb2b06ea2831693d
Fernando Salom <fer@rudo.es>
2024-02-09 10:19:56 +0100

    Add main interceptor

--- moshimoshi/src/main/java/com/moshimoshi/network/interceptor/AuthInterceptor.kt
+++ moshimoshi/src/main/java/com/moshimoshi/network/interceptor/AuthInterceptor.kt
@@ -1,13 +1,25 @@
 package com.moshimoshi.network.interceptor
 
 import com.moshimoshi.network.authenticator.Authenticator
+import com.moshimoshi.network.retrofit.authenticated
 import okhttp3.Interceptor
 import okhttp3.Response
 
 class AuthInterceptor(private val authenticator: Authenticator) : Interceptor {
 
     override fun intercept(chain: Interceptor.Chain): Response {
-        var request = authenticator.authorize(request = chain.request().newBuilder())
-        return chain.proceed(request.build())
+        val originRequest = chain.request()
+        val builder = originRequest.newBuilder()
+
+        return when {
+            originRequest.authenticated() -> {
+                var currentRequest = chain.request()
+                var request = authenticator.authorize(request = currentRequest.newBuilder())
+                chain.proceed(request.build())
+            }
+            else -> {
+                chain.proceed(builder.build())
+            }
+        }
     }
 }
