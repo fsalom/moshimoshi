0a2d7d55b2556305e6bd9664ea2074f05ab5f82d
Fernando Salom <fer@rudo.es>
2024-02-09 10:21:28 +0100

    Change getCurrentToken and refreshAccess interface

--- moshimoshi/src/main/java/com/moshimoshi/network/authenticationcard/AuthenticationCard.kt
+++ moshimoshi/src/main/java/com/moshimoshi/network/authenticationcard/AuthenticationCard.kt
@@ -1,8 +1,9 @@
 package com.moshimoshi.network.authenticationcard
+import com.moshimoshi.network.entities.Parameter
 import com.moshimoshi.network.entities.Tokens
 
 interface AuthenticationCard {
-    suspend fun getCurrentToken(): Tokens
-    suspend fun refreshAccessToken(refreshToken: String): Tokens
+    suspend fun getCurrentToken(parameters: List<Parameter>): Tokens?
+    suspend fun refreshAccessToken(refreshToken: String): Tokens?
     suspend fun logout()
 }
