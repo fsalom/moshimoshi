bd317ee6fb6bafdf690776c0304f562773c3ab9d
Fernando Salom <fer@rudo.es>
2024-02-09 10:20:59 +0100

    Fix authentication card with API

--- moshimoshi/src/main/java/com/moshimoshi/network/authenticationcard/api/APIAuthenticationImpl.kt
+++ moshimoshi/src/main/java/com/moshimoshi/network/authenticationcard/api/APIAuthenticationImpl.kt
@@ -2,33 +2,64 @@ package com.moshimoshi.network.authenticationcard.api
 
 import android.content.Context
 import android.content.Intent
-import androidx.core.content.ContextCompat.startActivity
 import com.google.gson.GsonBuilder
 import com.moshimoshi.network.authenticationcard.AuthenticationCard
+import com.moshimoshi.network.authenticationcard.api.dto.TokensDTO
+import com.moshimoshi.network.authenticationcard.api.dto.toDomain
+import com.moshimoshi.network.entities.Endpoint
+import com.moshimoshi.network.entities.Parameter
 import com.moshimoshi.network.entities.Tokens
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
-import okhttp3.Request
 
 
-class APIAuthenticationImpl(private val loginCall: Request,
-                            private val refreshCall: Request): AuthenticationCard {
+class APIAuthenticationImpl(private var loginEndpoint: Endpoint,
+                            private var refreshEndpoint: Endpoint,
+                            private val packageName: String,
+                            private val className: String,
+                            private val context: Context): AuthenticationCard {
 
-    override suspend fun getCurrentToken(): Tokens {
-        val response = OkHttpClient().newCall(loginCall).execute()
-        val gson = GsonBuilder().create()
-        return gson.fromJson(response.body()?.string(), Tokens::class.java)
+    override suspend fun getCurrentToken(parameters: List<Parameter>): Tokens? {
+        return withContext(Dispatchers.IO) {
+            try {
+                var completeLoginEndpoint = loginEndpoint
+                completeLoginEndpoint.add(formParams = parameters)
+                val request = completeLoginEndpoint.getRequest()
+                val response = OkHttpClient().newCall(request).execute()
+                val gson = GsonBuilder().create()
+                val tokensDTO = gson.fromJson(response.body()?.string(), TokensDTO::class.java)
+                tokensDTO.toDomain()
+            } catch (e: Exception) {
+                null
+            }
+        }
     }
 
-    override suspend fun refreshAccessToken(refreshToken: String): Tokens {
-        val response = OkHttpClient().newCall(refreshCall).execute()
-        val gson = GsonBuilder().create()
-        return gson.fromJson(response.body()?.string(), Tokens::class.java)
+    override suspend fun refreshAccessToken(refreshToken: String): Tokens? {
+        return withContext(Dispatchers.IO) {
+            try {
+                var completeRefreshEndpoint = refreshEndpoint
+                var refreshParameter = Parameter(key = "refresh_token", value = refreshToken)
+                completeRefreshEndpoint.add(formParams = listOf<Parameter>(refreshParameter))
+                val request = completeRefreshEndpoint.getRequest()
+                val response = OkHttpClient().newCall(request).execute()
+                val gson = GsonBuilder().create()
+                val tokensDTO = gson.fromJson(response.body()?.string(), TokensDTO::class.java)
+                tokensDTO.toDomain()
+            } catch (e: Exception) {
+                null
+            }
+        }
     }
 
     override suspend fun logout() {
-
+        showLogin()
     }
 
-    fun showLogin() {
+    private fun showLogin() {
+        val intent = Intent()
+        intent.setClassName(packageName, className)
+        context.startActivity(intent)
     }
 }
