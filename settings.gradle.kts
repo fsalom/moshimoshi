a565e538a2886ee8a347e9be00a3ee7f12a911b5
Fernando Salom <fer@rudo.es>
2024-03-17 12:16:25 +0100

    Fix several files related with jitpack

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,20 +1,3 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://jitpack.io")
-    }
-}
-
 rootProject.name = "MoshiMoshi"
 include(":app")
 include(":moshimoshi")
