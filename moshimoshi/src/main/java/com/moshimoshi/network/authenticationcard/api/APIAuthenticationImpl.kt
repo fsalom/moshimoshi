f717bee75076ee29ffd382eb28cddf72c741f011
Fernando Salom <fer@rudo.es>
2024-04-11 10:02:33 -0400

    Change interface and implementation of AuthenticationCard to return Tokens or throw with detail error

--- moshimoshi/src/main/java/com/moshimoshi/network/authenticationcard/api/APIAuthenticationImpl.kt
+++ moshimoshi/src/main/java/com/moshimoshi/network/authenticationcard/api/APIAuthenticationImpl.kt
@@ -7,12 +7,14 @@ import com.moshimoshi.network.authenticationcard.AuthenticationCard
 import com.moshimoshi.network.authenticationcard.api.dto.TokensDTO
 import com.moshimoshi.network.authenticationcard.api.dto.toDomain
 import com.moshimoshi.network.entities.Endpoint
+import com.moshimoshi.network.entities.NetworkError
 import com.moshimoshi.network.entities.Parameter
+import com.moshimoshi.network.entities.Token
 import com.moshimoshi.network.entities.Tokens
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
-
+import okhttp3.Response
 
 class APIAuthenticationImpl(private var loginEndpoint: Endpoint,
                             private var refreshEndpoint: Endpoint,
@@ -20,36 +22,24 @@ class APIAuthenticationImpl(private var loginEndpoint: Endpoint,
                             private val className: String,
                             private val context: Context): AuthenticationCard {
 
-    override suspend fun getCurrentToken(parameters: List<Parameter>): Tokens? {
+    override suspend fun getCurrentToken(parameters: List<Parameter>): Tokens {
         return withContext(Dispatchers.IO) {
-            try {
-                var completeLoginEndpoint = loginEndpoint
-                completeLoginEndpoint.add(params = parameters)
-                val request = completeLoginEndpoint.getRequest()
-                val response = OkHttpClient().newCall(request).execute()
-                val gson = GsonBuilder().create()
-                val tokensDTO = gson.fromJson(response.body()?.string(), TokensDTO::class.java)
-                tokensDTO.toDomain()
-            } catch (e: Exception) {
-                null
-            }
+            val completeLoginEndpoint = loginEndpoint
+            completeLoginEndpoint.add(params = parameters)
+            val request = completeLoginEndpoint.getRequest()
+            val response = OkHttpClient().newCall(request).execute()
+            getTokensOrFail(response)
         }
     }
 
-    override suspend fun refreshAccessToken(refreshToken: String): Tokens? {
+    override suspend fun refreshAccessToken(refreshToken: String): Tokens {
         return withContext(Dispatchers.IO) {
-            try {
-                var completeRefreshEndpoint = refreshEndpoint
-                var refreshParameter = Parameter(key = "refresh_token", value = refreshToken)
-                completeRefreshEndpoint.add(params = listOf<Parameter>(refreshParameter))
-                val request = completeRefreshEndpoint.getRequest()
-                val response = OkHttpClient().newCall(request).execute()
-                val gson = GsonBuilder().create()
-                val tokensDTO = gson.fromJson(response.body()?.string(), TokensDTO::class.java)
-                tokensDTO.toDomain()
-            } catch (e: Exception) {
-                null
-            }
+            val completeRefreshEndpoint = refreshEndpoint
+            val refreshParameter = Parameter(key = "refresh_token", value = refreshToken)
+            completeRefreshEndpoint.add(params = listOf<Parameter>(refreshParameter))
+            val request = completeRefreshEndpoint.getRequest()
+            val response = OkHttpClient().newCall(request).execute()
+            getTokensOrFail(response)
         }
     }
 
@@ -57,6 +47,17 @@ class APIAuthenticationImpl(private var loginEndpoint: Endpoint,
         showLogin()
     }
 
+    private fun getTokensOrFail(response: Response): Tokens {
+        return if (response.isSuccessful) {
+            val gson = GsonBuilder().create()
+            val tokensDTO = gson.fromJson(response.body()?.string(), TokensDTO::class.java)
+            tokensDTO.toDomain()
+        } else {
+            val code = response.code()
+            val body = response.body()?.string()
+            throw NetworkError.Failure(code, body ?: "")
+        }
+    }
     private fun showLogin() {
         val intent = Intent()
         intent.setClassName(packageName, className)
