c81ac5d3b77e53383b2aa9252ca213128c67e470
Fernando Salom <fer@rudo.es>
2024-04-16 15:50:58 +0200

    Add mutex for sync access to authorize

--- moshimoshi/src/main/java/com/moshimoshi/network/interceptor/auth/AuthInterceptor.kt
+++ moshimoshi/src/main/java/com/moshimoshi/network/interceptor/auth/AuthInterceptor.kt
@@ -3,21 +3,26 @@ package com.moshimoshi.network.interceptor.auth
 import com.moshimoshi.network.authenticator.Authenticator
 import com.moshimoshi.network.retrofit.authenticated
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import okhttp3.Interceptor
 import okhttp3.Response
 
 class AuthInterceptor(private val authenticator: Authenticator) : Interceptor {
-
+    private val authorizationMutex = Mutex()
     override fun intercept(chain: Interceptor.Chain): Response {
         val originRequest = chain.request()
         val requestWithHeaders = authenticator.addHeaders(originRequest.newBuilder())
 
         return when {
             originRequest.authenticated() -> {
-                var requestAuthenticated = runBlocking {
-                    authenticator.authorize(request = requestWithHeaders)
+                runBlocking {
+                    authorizationMutex.withLock {
+                        authenticator.authorize(requestWithHeaders)
+                    }
+                }.let { requestAuthenticated ->
+                    chain.proceed(requestAuthenticated.build())
                 }
-                chain.proceed(requestAuthenticated.build())
             }
             else -> {
                 chain.proceed(requestWithHeaders.build())
