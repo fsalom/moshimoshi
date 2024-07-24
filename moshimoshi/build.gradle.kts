ec4ff937e917588ea84a932ce54365ae8e49e3a8
Fernando Salom <fer@rudo.es>
2024-07-24 10:51:44 +0200

    Update build.gradle.kts

--- moshimoshi/build.gradle.kts
+++ moshimoshi/build.gradle.kts
@@ -48,7 +48,7 @@ afterEvaluate {
                     from (components["release"])
                     groupId = "com.github.fsalom"
                     artifactId = "moshimoshi"
-                    version = "1.1.5.3"
+                    version = "1.1.6.4"
                 }
             }
         }
