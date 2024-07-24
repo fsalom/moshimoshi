2a9b98d183a2fc2dbe14648c99fdb4ac4cde1eef
Fernando Salom <fer@rudo.es>
2024-07-24 10:38:28 +0200

    Fix problem when expiresIn is not part of JSON

--- moshimoshi/src/main/java/com/moshimoshi/network/authenticationcard/api/dto/TokensDTO.kt
+++ moshimoshi/src/main/java/com/moshimoshi/network/authenticationcard/api/dto/TokensDTO.kt
@@ -6,13 +6,17 @@ import com.moshimoshi.network.entities.Tokens
 
 data class TokensDTO(
     @SerializedName("access_token") val accessToken: String,
-    @SerializedName("expires_in") val expiresIn: Int,
+    @SerializedName("expires_in") val expiresIn: Int?,
     @SerializedName("refresh_token") val refreshToken: String?
 )
 
 fun TokensDTO.toDomain(): Tokens {
+    var expireTime = 31536000
+    if (expiresIn != null) {
+        expireTime = expiresIn
+    }
     return Tokens(
-        accessToken = Token(value = accessToken, timestampExpires = System.currentTimeMillis() + expiresIn * 1000),
+        accessToken = Token(value = accessToken, timestampExpires = System.currentTimeMillis() + expireTime * 1000),
         //Refresh token expiration time by default is 365 days
         refreshToken = refreshToken?.let { Token(value = it, timestampExpires = System.currentTimeMillis() + 31536000 * 1000) }
     )
